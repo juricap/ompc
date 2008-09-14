f95381f1e3d4ccc4a1c2e93217aed06029da65aa
juricap <juricap@JurzTablet>
2008-09-14 23:21:05 +0900

    mex examples made cleaner

--- examples/mex/compile.bat
+++ examples/mex/compile.bat
@@ -1,5 +1,8 @@
 cl /c /EHsc mexlib.cpp
 cl /c /EHsc matrix.cpp
 lib mexlib.obj matrix.obj
+
 cl /c /EHcs /I. derivate.cpp
+cl /c /EHcs /I. lineintc.cpp
+link /DLL /EXPORT:mexFunction lineintc.obj mexlib.lib
 link /DLL /EXPORT:mexFunction derivate.obj mexlib.lib
